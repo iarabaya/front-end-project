6401b6a12593a7e8a03a846d61dc334df01676d3
iara baya <iarabayav@gmail.com>
2022-04-21 18:37:08 -0300

    profile class component

--- src/components/classes/Profile.js
+++ src/components/classes/Profile.js
@@ -6,8 +6,8 @@ class Profile extends React.Component {
     this.state = { username: this.props.username || "Unidentified user" };
   }
 
-  //contenidos dentro del perfil
-  // avatar, username, alias, location, pokemons
+  //contenidos dentro del perfil de entrenador pokemon
+  // avatar, username, alias, home, region, pokemon_team, type, rank, medals
   render() {
     return (<>
       <h1>{this.state.username}</h1>
